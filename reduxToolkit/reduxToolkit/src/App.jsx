f144520987999a551d790a25b21d838f595afc5c
Samruddhi-techenthusiast <samru2304@gmail.com>
2026-01-26 19:56:01 +0530

    Added new project reduxToolkit

---
+++ reduxToolkit/reduxToolkit/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+
+
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
+
+
+function App() {
+const [count, setCount] = useState(0);
+
+
+return (
+<>
+<h1>Learn About Redux Toolkit</h1>
+<AddTodo />
+<Todos />
+</>
+);
+}
+
+
+export default App;
